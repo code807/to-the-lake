9fb5ed867d3fe2d9077fba0176a56fd8f8c31648
quellus <quellus7@gmail.com>
2024-06-14 16:31:09 -0600

    Added basic player controller and standin art for player

---
+++ CharacterController.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const MOVEMENT_SMOOTHING = 20
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction:
+		direction *= SPEED
+		velocity = velocity.move_toward(direction, MOVEMENT_SMOOTHING)
+	else:
+		velocity =  velocity.move_toward(Vector2(0,0), MOVEMENT_SMOOTHING)
+
+	move_and_slide()
