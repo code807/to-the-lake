0f44738a47e5ebde8ff5be7e33588f50a3e65cf0
code807 <noahcrandall.m@gmail.com>
2024-06-14 18:01:43 -0700

    add player sprite animation

--- CharacterController.gd
+++ CharacterController.gd
@@ -1,7 +1,12 @@
 class_name Player extends CharacterBody2D
 
-const SPEED = 200.0
-const MOVEMENT_SMOOTHING = 50
+const SPEED = 75.0
+const MOVEMENT_SMOOTHING = 10
+const STOP_SMOOTHING = 50
+@onready var sprite = $AnimatedSprite2D
+
+var animindex = 1
+var animlist = ["walk_up", "walk_left", "walk_right", "walk_down"]
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,9 +16,23 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
+		if not sprite.is_playing():
+			sprite.play()
+		var strongaxis = direction.max_axis_index()
+		var mindex = strongaxis+2 if sign(direction[strongaxis]) > 0 else strongaxis
+		if mindex != animindex:
+			print(mindex)
+			animindex = mindex
+			sprite.animation = animlist[animindex]
+			sprite.play()
 		direction *= SPEED
 		velocity = velocity.move_toward(direction, MOVEMENT_SMOOTHING)
 	else:
-		velocity =  velocity.move_toward(Vector2(0,0), MOVEMENT_SMOOTHING)
-
+		velocity =  velocity.move_toward(Vector2(0,0), STOP_SMOOTHING)
 	move_and_slide()
+
+func _checkanim():
+	print("play")
+	if sprite.frame%3 == 0 and velocity.length() <= 5:
+		sprite.frame = 0
+		sprite.stop()
