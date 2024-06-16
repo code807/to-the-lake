e902c40403a8a2b70cf09cc6ca8ac65955b551ad
quellus <quellus7@gmail.com>
2024-06-15 19:45:55 -0600

    added dialogue manager to support multiple characters and queueing

---
+++ dialogue_resource.gd
@@ -0,0 +1,6 @@
+class_name DialogResource extends Resource
+
+@export var story_teller: StoryTellers.names
+@export var dialogue: String
+@export var speech_time: float
+@export var wait_time: float
