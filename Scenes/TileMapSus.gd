493a7435d0dc881c078246caaa8b61bf6b8bed18
code807 <code807@protonmail.com>
2024-06-18 21:50:38 -0700

    animate water texture

--- Scenes/TileMapSus.gd
+++ Scenes/TileMapSus.gd
@@ -1,14 +1,16 @@
+@tool
 extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func aaa():
 	var tiles: TileSetAtlasSource = tile_set.get_source(tile_set.get_source_id(3))
 	
 	for y in range(49):
 		tiles.set_tile_animation_columns(Vector2i(0, y), 11)
 		tiles.set_tile_animation_frames_count (Vector2i(0, y), 11)
 		tiles.set_tile_animation_speed(Vector2i(0, y), 5)
+	print("I tried")
 	pass # Replace with function body.
 
 
