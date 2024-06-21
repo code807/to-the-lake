89b653af2ffc382774f887c0542436f1d0a70a10
code807 <code807@protonmail.com>
2024-06-21 16:54:41 -0700

    add particles and create puzzle level 2

---
+++ Scenes/Particle Emitter.gd
@@ -0,0 +1,24 @@
+extends AnimatedSprite2D
+
+var timer = Timer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation = "1"
+	add_child(timer)
+	timer.connect("timeout", timerstuff)
+	timerstuff()
+	for m in range(PI):
+		print(m)
+	pass # Replace with function body.
+
+func timerstuff():
+	if randf() > 0.5:
+		if randf() > 0.5:
+			play("1")
+		else:
+			play("2")
+	timer.start(randf_range(4, 8))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
