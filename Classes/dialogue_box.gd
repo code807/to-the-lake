74c220612feea7c043f2423753b2e7837478f569
code807 <code807@protonmail.com>
2024-06-20 21:34:28 -0700

    alter text visuals

--- Classes/dialogue_box.gd
+++ Classes/dialogue_box.gd
@@ -16,7 +16,15 @@ func _ready():
 
 func play_dialogue(dialogue: String, speech_time: float, pause_time: float):
 	print("Dialog box is playing " + dialogue + " for time " + str(speech_time))
-	label.text = ""
+	match storyteller:
+		StoryTellers.names.CHARLIE:
+			label.text = "[color=#2482be]Jera[/color]\n"
+		StoryTellers.names.TAYLOR:
+			label.text = "[right][color=#9f24be]Dove[/color]\n[right]"
+		StoryTellers.names.JEFF:
+			label.text = "[color=#e69127]Charlie[/color]\n"
+		StoryTellers.names.CHRIS:
+			label.text = "[right][color=#fd8d00]Jak[/color]\n[right]"
 	string = dialogue
 	label.visible = true
 	timer.start(speech_time+pause_time)
@@ -38,11 +46,11 @@ func _on_finished():
 	label.size.y = label.get_content_height()
 	match(storyteller):
 		StoryTellers.names.CHARLIE:
-			label.set_position(Vector2(6,6))
+			label.set_position(Vector2(5,2))
 		StoryTellers.names.TAYLOR:
-			label.set_position(Vector2(size.x-label.size.x-6,6))
+			label.set_position(Vector2(size.x-label.size.x-5,2))
 		StoryTellers.names.JEFF:
-			label.set_position(Vector2(size.x-label.size.x-6, size.y-label.size.y-12))
+			label.set_position(Vector2(size.x-label.size.x-5, size.y-label.size.y-5))
 		StoryTellers.names.CHRIS:
-			label.set_position(Vector2(6, size.y-label.size.y-12))
+			label.set_position(Vector2(5, size.y-label.size.y-5))
 	
